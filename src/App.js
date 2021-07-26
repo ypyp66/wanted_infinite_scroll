106c7fd66f0770c1e57592d3b4b6bdf102f1f5f5
kitebeen <ypyp66@naver.com>
2021-07-26 22:19:29 +0900

    feat_k:레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+import CardList from './components/CardList';
+
+const Conatiner = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 function App() {
-  return <div>asda</div>;
+  return <CardList />;
 }
 
 export default App;
--- src/components/CardListItem.js
+++ src/components/CardListItem.js
@@ -1,5 +1,52 @@
-function CardListItem() {
-  return <div>asda</div>;
+import React from 'react';
+import styled from 'styled-components';
+
+const ItemContainer = styled.div`
+  width: 500px;
+  align-items: flex-start;
+  background-color: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  border-radius: 20px;
+  box-sizing: border-box;
+  padding: 20px;
+
+  & + & {
+    margin-top: 14px;
+  }
+`;
+
+const Item = styled.div`
+  & + & {
+    margin-top: 12px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 18px;
+  line-height: 21px;
+  font-weight: bold;
+  margin-right: 12px;
+`;
+
+function CardListItem({ data }) {
+  return (
+    <ItemContainer>
+      <Item>
+        <Title>Comment Id</Title>
+        {data.id}
+      </Item>
+      <Item>
+        <Title>Email</Title>
+        {data.email}
+      </Item>
+      <Item>
+        <div>
+          <Title>Comment</Title>
+        </div>
+        {data.body}
+      </Item>
+    </ItemContainer>
+  );
 }
 
 export default CardListItem;
