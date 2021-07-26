ec6c8d8d6917ca0b301731ed67c406f3ce4f5806
kitebeen <ypyp66@naver.com>
2021-07-26 22:22:21 +0900

    fix_k: App.js styled 삭제

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import styled from 'styled-components';
 import CardList from './components/CardList';
-
-const Conatiner = styled.div`
-  margin: 0 auto;
-  width: 100%;
-`;
-
-const Inner = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
 
 function App() {
   return <CardList />;
